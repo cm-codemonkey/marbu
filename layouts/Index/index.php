e2745db88a67a643b984761cd72aec82eb9b98ee
Gersón Gómez <gergomez18@gmail.com>
2021-02-24 03:54:49 -0500

    Actualización

--- layouts/Index/index.php
+++ layouts/Index/index.php
@@ -217,7 +217,7 @@ $this->dependencies->add(['js', '{$path.js}Index/index.js?v=1.0.0']);
             </div>
             <div class="col-12 col-lg-3 m-0 p-0">
                 <figure class="v d-flex align-items-center justify-content-center p-40">
-                    <img src="https://one-consultores.com/assets/images/clients/mapla.png">
+                    <img src="https://one-consultores.com/assets/images/clients/MAPLA.png">
                 </figure>
             </div>
             <div class="col-12 col-lg-3 m-0 p-0">
